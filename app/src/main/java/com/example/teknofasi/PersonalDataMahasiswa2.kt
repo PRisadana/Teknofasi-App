0a19cae6821037704f5637b10b031caf5f4d5ba3
PRisadana <resandyprisadana2323@gmail.com>
2024-05-16 10:16:05 +0800

    Memberikan Dropdown Gender

--- app/src/main/java/com/example/teknofasi/PersonalDataMahasiswa2.kt
+++ app/src/main/java/com/example/teknofasi/PersonalDataMahasiswa2.kt
@@ -3,26 +3,39 @@ package com.example.teknofasi
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Calendar
 
-class PersonalDataMahasiswa2 : AppCompatActivity() {
+class PersonalDataMahasiswa2() : AppCompatActivity(), Parcelable {
     private lateinit var panahkiriImageView: ImageView
     private lateinit var selesaiButton: Button
     private lateinit var tanggallahirEditText: EditText
+    private lateinit var genderSpinner: Spinner
 
     private lateinit var namaEditText: EditText
+
+    constructor(parcel: Parcel) : this() {
+
+    }
 
     private fun initComponents() {
         panahkiriImageView = findViewById(R.id.panahKiri)
         selesaiButton = findViewById(R.id.selesai)
         tanggallahirEditText = findViewById(R.id.tanggalLahirEditText)
+        genderSpinner = findViewById(R.id.genderSpinner)
 
         namaEditText = findViewById(R.id.namaAsliEditText)
     }
@@ -43,6 +56,24 @@ class PersonalDataMahasiswa2 : AppCompatActivity() {
 
         datePickerDialog.show()
 
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PersonalDataMahasiswa2> {
+        override fun createFromParcel(parcel: Parcel): PersonalDataMahasiswa2 {
+            return PersonalDataMahasiswa2(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PersonalDataMahasiswa2?> {
+            return arrayOfNulls(size)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,6 +109,38 @@ class PersonalDataMahasiswa2 : AppCompatActivity() {
             startActivity(intent)
         }
 
-    }
+        // Buat ArrayAdapter menggunakan array gender dan layout default spinner
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.gender_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Tentukan layout yang akan digunakan saat daftar pilihan muncul
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Terapkan adapter ke spinner
+            genderSpinner.adapter = adapter
+        }
 
+        genderSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedGender = parent.getItemAtPosition(position).toString()
+                Toast.makeText(
+                    this@PersonalDataMahasiswa2,
+                    "Selected gender: $selectedGender",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+
+        }
+
+    }
 }
